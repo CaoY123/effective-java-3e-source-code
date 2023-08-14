8730fd5a9261385f23b01eb5abf660be7804efe7
CaoY123456 <caoy_6666@126.com>
2023-08-14 16:20:12 +0800

    枚举和注解第38、39、40、41条的学习

--- src/effectivejava/chapter6/item39/annotationwitharrayparameter/RunTests.java
+++ src/effectivejava/chapter6/item39/annotationwitharrayparameter/RunTests.java
@@ -8,7 +8,8 @@ public class RunTests {
     public static void main(String[] args) throws Exception {
         int tests = 0;
         int passed = 0;
-        Class<?> testClass = Class.forName(args[0]);
+        String testClassName = "effectivejava.chapter6.item39.annotationwitharrayparameter.Sample3";
+        Class<?> testClass = Class.forName(testClassName);
         for (Method m : testClass.getDeclaredMethods()) {
             if (m.isAnnotationPresent(Test.class)) {
                 tests++;
--- src/effectivejava/chapter6/item39/repeatableannotation/RunTests.java
+++ src/effectivejava/chapter6/item39/repeatableannotation/RunTests.java
@@ -10,7 +10,8 @@ public class RunTests {
     public static void main(String[] args) throws Exception {
         int tests = 0;
         int passed = 0;
-        Class testClass = Class.forName(args[0]);
+        String testClassName = "effectivejava.chapter6.item39.repeatableannotation.Sample4";
+        Class testClass = Class.forName(testClassName);
         for (Method m : testClass.getDeclaredMethods()) {
             if (m.isAnnotationPresent(Test.class)) {
                 tests++;
